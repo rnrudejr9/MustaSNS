935375e61a7d86a3d071658a67af4668c2c8aede
pi koo <rnrudejr1234@naver.com>
2022-12-20 13:59:30 +0900

    UserController.java -> HelloController.java 수정

--- src/main/java/com/example/mutsasnsproject/controller/HelloController.java
+++ src/main/java/com/example/mutsasnsproject/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.example.mutsasnsproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/")
 @Slf4j
-public class UserController {
+public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello(){
-        return ResponseEntity.ok().body("bye");
+        return ResponseEntity.ok().body("hello");
     }
 
 }

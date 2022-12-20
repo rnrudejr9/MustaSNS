ee99293e9031250d90c6a8e4331e81ae8e464c9f
pi koo <rnrudejr1234@naver.com>
2022-12-20 10:10:04 +0900

    초기 셋팅 1. swagger 설정 2. yml 파일 설정

---
+++ src/test/java/com/example/mutsasnsproject/MutsaSnsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutsasnsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutsaSnsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

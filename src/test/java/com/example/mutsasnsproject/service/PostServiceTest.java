83b1232f0559fac37250a5f45daa23e7cf6d0d18
pi koo <rnrudejr1234@naver.com>
2022-12-27 17:24:21 +0900

    테스트 코드 반영 1. 회원가입 기능 2. 댓글 작성 및 좋아요 기능

--- src/test/java/com/example/mutsasnsproject/service/PostServiceTest.java
+++ src/test/java/com/example/mutsasnsproject/service/PostServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.mutsasnsproject.service;
 
+import com.example.mutsasnsproject.domain.dto.comment.CommentResponse;
 import com.example.mutsasnsproject.domain.dto.post.PostDetailResponse;
 import com.example.mutsasnsproject.domain.dto.post.PostRequest;
 import com.example.mutsasnsproject.domain.entity.Post;
@@ -9,6 +10,8 @@ import com.example.mutsasnsproject.exception.ErrorCode;
 import com.example.mutsasnsproject.fixture.PostEntityFixture;
 import com.example.mutsasnsproject.fixture.TestInfoFixture;
 import com.example.mutsasnsproject.fixture.UserEntityFixture;
+import com.example.mutsasnsproject.repository.CommentRepository;
+import com.example.mutsasnsproject.repository.GoodRepository;
 import com.example.mutsasnsproject.repository.PostRepository;
 import com.example.mutsasnsproject.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -36,12 +39,31 @@ public class PostServiceTest {
     PostRepository postRepository = mock(PostRepository.class);
     @Mock
     UserRepository userRepository = mock(UserRepository.class);
+    @Mock
+    CommentRepository commentRepository = mock(CommentRepository.class);
+
+    @Mock
+    GoodRepository goodRepository = mock(GoodRepository.class);
 
     @BeforeEach
     void setUp() {
-        postService = new PostService(postRepository, userRepository);
+        postService = new PostService(postRepository, userRepository,commentRepository,goodRepository);
     }
 
+//    테스트 코드
+//    ----------------------------------------------------------------------------
+//    게시글 등록 성공)
+//    게시글 등록 실패) 유저존재하지않음
+//    게시글 조회 성공)
+//    게시글 삭제 성공)
+//    게시글 삭제 실패) 유저 존재하지 않음
+//    게시글 삭제 실패) 삭제할 게시글이 없음
+//    게시글 삭제 실패) 삭제할 게시글의 작성자와 사용자가 다름
+//    게시글 수정 성공)
+//    게시글 수정 실패) 유저 존재하지 않음
+//    게시글 수정 실패) 수정할 게시글이 없음
+//    게시글 수정 실패) 수정할 게시글의 작성자와 사용자가 다름
+//    -----------------------------------------------------------------------------
 
 
     @Test
@@ -165,7 +187,7 @@ public class PostServiceTest {
         assertEquals(ErrorCode.USERNAME_NOT_FOUND,exception.getErrorCode());
     }
     @Test
-    @DisplayName("삭제) 실패 : 수정할 포스트가 없을 경우")
+    @DisplayName("삭제) 실패 : 삭제할 포스트가 없을 경우")
     void delete_fail2(){
         TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
         User user = mock(User.class);
@@ -177,7 +199,7 @@ public class PostServiceTest {
         assertEquals(ErrorCode.POST_NOT_FOUND,exception.getErrorCode());
     }
     @Test
-    @DisplayName("삭제) 실패 : 사용자와 수정할 포스트의 작성자가 다를 경우")
+    @DisplayName("삭제) 실패 : 사용자와 삭제할 포스트의 작성자가 다를 경우")
     void delete_fail_3(){
         TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
         User user = UserEntityFixture.get("user1","password");
@@ -191,12 +213,6 @@ public class PostServiceTest {
         //post의 작성자와 user의 id값과 다르기 때문에 에러 코드 맞음
         assertEquals(ErrorCode.INVALID_PERMISSION,exception.getErrorCode());
     }
-
-
-
-
-
-
 
 
 }
